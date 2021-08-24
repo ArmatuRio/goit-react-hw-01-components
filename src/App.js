70926be346ce02069de1ae6d20432a40a1b5336d
Артур Евсеев <75883199+ArmatuRio@users.noreply.github.com>
2021-08-24 12:45:47 +0300

    add file

---
+++ src/App.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import Profile from './components/Profile/Profile';
+import user from './components/Profile/user.json';
+import Statistics from './components/Statistics/Statistics';
+import statisticalData from './components/Statistics/statistical-data.json';
+import FriendList from './components/FriendList/FriendList';
+import friends from './components/FriendList/friends.json';
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transactions from './components/TransactionHistory/transactions.json';
+
+const App = () => (
+  <>
+    <Profile
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
+    <Statistics title="Upload stats" stats={statisticalData} />
+    <FriendList friends={friends} />
+    <TransactionHistory items={transactions} />
+  </>
+);
+
+export default App;
+ 
